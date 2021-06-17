94498a0e16b5e5e11bd07cd2b02e15c1069a50ff
camelot1399 <camelot1399@mail.ru>
2021-06-18 00:05:11 +0300

    day1; add 2 components

---
+++ src/components/formComponent.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <button @click="form = !form" class="btn">Form</button>
+        <form action="#" v-if="form">
+            <input type="text" name="name" v-model="name" placeholder="name">
+            <input type="number" name="number" v-model="age" placeholder="age">
+            <button @click.prevent="add">add</button>
+        </form>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            name: '',
+            age: 20,
+            form: false
+        }
+    },
+    methods: {
+        add() {
+            let {name, age} = this;
+            this.$emit('add', {name, age});
+        }
+    }
+}
+</script>
+<style>
+    .btn {
+        background: red;
+    }
+</style>
